d8e575582ec283e66855455ab0b8d556f31013e4
Mahroof VP <mahroofvp77@gmail.com>
2024-11-03 17:05:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import './App.css'
+import WhiteLogo from '../src/assets/LogoWhite.webp'
+function App() {
+  return (
+    <>
+      
+    <div className="main-div" >
+
+      <div className="white-logo-div">
+        <div className="white-logo-contants">
+        <img src={WhiteLogo} alt="" className="white-logo-img" />
+        <h3>CHARTBY</h3>
+        <h6>TRADING JOURNAL</h6>
+        </div>
+      </div>
+
+      <div className="soon-text-div" >
+        <div className="soon-text">
+        <h1>COMING SOON</h1>
+        </div>
+      </div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default App
